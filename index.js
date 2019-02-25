ddc35d87056f104e6fde6d50561983b50ad391ab
nabarun <nabaruns@050102R3974.local>
2019-02-25 17:58:40 +0530

    Added my project

---
+++ index.js
@@ -0,0 +1,13 @@
+const express=require('express');
+const app=express();
+
+app.get('/',(req,res)=>{
+res.send('Say Hello');
+});
+
+app.listen(3000,()=>{
+    console.log('Listening on port 3000');
+});
+
+    
+
